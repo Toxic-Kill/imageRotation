bea7e81dada53e255847ab7be7f89d6bd274b456
Kill Toxic <913087364@qq.com>
2020-04-10 14:51:26 +0800

    imageRotation

--- imageRotation/imageRotation.cpp
+++ imageRotation/imageRotation.cpp
@@ -1,20 +1,31 @@
-﻿// imageRotation.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\Files\\lena.jpg");//读取图像
+	//检测是否读取成功
+	if (srcMat.empty())
+	{
+		return -1;
+	}
+	cv::Point2f center(srcMat.cols / 2.0, srcMat.rows / 2.0);
+	//旋转-10℃，缩放尺度为
+	float angle = -10, scale = 1;
+	cv::Mat rot = cv::getRotationMatrix2D(center, angle, scale);
+	//获取外界四边形
+	cv::Rect bbox = RotatedRect(center, srcMat.size(), angle).boundingRect();
+	//调整仿射矩形参数
+	rot.at<double>(0, 2) += bbox.width / 2.0 - center.x;
+	rot.at<double>(1, 2) += bbox.height / 2.0 - center.y;
+	//仿射变换
+	cv::warpAffine(srcMat, dstMat, rot, bbox.size());
+	//显示结果
+	cv::imshow("src", srcMat);
+	cv::imshow("dst", dstMat);
+	waitKey(0);
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
